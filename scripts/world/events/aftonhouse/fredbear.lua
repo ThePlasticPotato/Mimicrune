8d497e7644e79696edfa98c4e20a0e8b8db2fbd2
ThePlasticPotato <theplasticpotato@gmail.com>
2025-07-31 23:26:59 -0400

    evan voice update

--- scripts/world/events/aftonhouse/fredbear.lua
+++ scripts/world/events/aftonhouse/fredbear.lua
@@ -1,14 +1,19 @@
 local FredbearPlushie, super = Class(Event)
 
 function FredbearPlushie:init(data)
-    if (Game:getFlag("fredbear_get, false") ~= true) then
-        super.init(self, data.x, data.y - 30, 16, 21, data)
-        self:setSprite("party/fredbear/light")
+    super.init(self, data.x, data.y - 30, 16, 21, data)
+    self:setSprite("party/fredbear/light")
+    if (Game:getFlag("fredbear_get", false)) then
+        self.visible = false
     end
 end
 
 function FredbearPlushie:onInteract(player, dir)
     super.onInteract(self, player, dir)
+    if (Game:getFlag("fredbear_get", false)) then
+        self:remove()
+        return true
+    end
     Game:setFlag("fredbear_get", true)
     Assets.playSound("noise")
     self.visible = false
