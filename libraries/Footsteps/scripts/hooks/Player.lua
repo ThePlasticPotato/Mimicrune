25ece81c55ab130e374ba0b11bcba7e86146c5f0
ThePlasticPotato <theplasticpotato@gmail.com>
2025-09-29 13:39:34 -0400

    fix crash pertaining to weird hook i still havent removed

--- libraries/Footsteps/scripts/hooks/Player.lua
+++ libraries/Footsteps/scripts/hooks/Player.lua
@@ -2,7 +2,7 @@
 local Player, super = Utils.hookScript(Player)
 
 function Player:getStepVolume()
-    return math.min(Footsteps:getConfig("step_volume") * (self.walk_speed / 4), Footsteps:getConfig("step_volume_max"))
+    return math.min(Footsteps:getConfig("step_volume") * (self:getCurrentSpeed(self.state == "RUN") / 4), Footsteps:getConfig("step_volume_max"))
 end
 
 return Player
