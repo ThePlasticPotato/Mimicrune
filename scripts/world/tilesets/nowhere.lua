cf4177a69f0acec9d9c5f3d3194f3950a8dbd94f
ThePlasticPotato <theplasticpotato@gmail.com>
2026-04-03 13:11:57 -0400

    update nowhere tiles

--- scripts/world/tilesets/nowhere.lua
+++ scripts/world/tilesets/nowhere.lua
@@ -25,7 +25,187 @@ return {
     height = 20
   },
   properties = {},
-  wangsets = {},
+  wangsets = {
+    {
+      name = "Base Tiles",
+      class = "",
+      tile = -1,
+      wangsettype = "corner",
+      properties = {},
+      colors = {
+        {
+          color = { 255, 0, 0 },
+          name = "Turfle",
+          class = "",
+          probability = 1,
+          tile = -1,
+          properties = {}
+        },
+        {
+          color = { 0, 255, 0 },
+          name = "Blackglass",
+          class = "",
+          probability = 1,
+          tile = -1,
+          properties = {}
+        }
+      },
+      wangtiles = {
+        {
+          wangid = { 0, 0, 0, 1, 0, 0, 0, 0 },
+          tileid = 0
+        },
+        {
+          wangid = { 0, 0, 0, 1, 0, 1, 0, 0 },
+          tileid = 1
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 1, 0, 0 },
+          tileid = 2
+        },
+        {
+          wangid = { 0, 0, 0, 2, 0, 1, 0, 0 },
+          tileid = 14
+        },
+        {
+          wangid = { 0, 0, 0, 1, 0, 2, 0, 0 },
+          tileid = 15
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 0, 0, 0 },
+          tileid = 17
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 1, 0, 1 },
+          tileid = 18
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 1, 0, 1 },
+          tileid = 19
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 1, 0, 0 },
+          tileid = 21
+        },
+        {
+          wangid = { 0, 0, 0, 1, 0, 1, 0, 1 },
+          tileid = 22
+        },
+        {
+          wangid = { 0, 2, 0, 2, 0, 1, 0, 1 },
+          tileid = 31
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 2, 0, 2 },
+          tileid = 32
+        },
+        {
+          wangid = { 0, 1, 0, 0, 0, 0, 0, 0 },
+          tileid = 34
+        },
+        {
+          wangid = { 0, 1, 0, 0, 0, 0, 0, 1 },
+          tileid = 35
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 0, 0, 1 },
+          tileid = 36
+        },
+        {
+          wangid = { 0, 1, 0, 1, 0, 0, 0, 1 },
+          tileid = 38
+        },
+        {
+          wangid = { 0, 1, 0, 0, 0, 1, 0, 1 },
+          tileid = 39
+        },
+        {
+          wangid = { 0, 2, 0, 0, 0, 0, 0, 1 },
+          tileid = 48
+        },
+        {
+          wangid = { 0, 1, 0, 0, 0, 0, 0, 2 },
+          tileid = 49
+        },
+        {
+          wangid = { 0, 0, 0, 2, 0, 0, 0, 0 },
+          tileid = 85
+        },
+        {
+          wangid = { 0, 0, 0, 2, 0, 2, 0, 0 },
+          tileid = 86
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 2, 0, 0 },
+          tileid = 87
+        },
+        {
+          wangid = { 0, 2, 0, 2, 0, 0, 0, 0 },
+          tileid = 102
+        },
+        {
+          wangid = { 0, 2, 0, 2, 0, 2, 0, 2 },
+          tileid = 103
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 2, 0, 2 },
+          tileid = 104
+        },
+        {
+          wangid = { 0, 2, 0, 2, 0, 2, 0, 0 },
+          tileid = 106
+        },
+        {
+          wangid = { 0, 0, 0, 2, 0, 2, 0, 2 },
+          tileid = 107
+        },
+        {
+          wangid = { 0, 2, 0, 0, 0, 0, 0, 0 },
+          tileid = 119
+        },
+        {
+          wangid = { 0, 2, 0, 0, 0, 0, 0, 2 },
+          tileid = 120
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 0, 0, 2 },
+          tileid = 121
+        },
+        {
+          wangid = { 0, 2, 0, 2, 0, 0, 0, 2 },
+          tileid = 123
+        },
+        {
+          wangid = { 0, 2, 0, 0, 0, 2, 0, 2 },
+          tileid = 124
+        },
+        {
+          wangid = { 0, 1, 0, 2, 0, 0, 0, 0 },
+          tileid = 150
+        },
+        {
+          wangid = { 0, 1, 0, 2, 0, 2, 0, 1 },
+          tileid = 151
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 2, 0, 1 },
+          tileid = 152
+        },
+        {
+          wangid = { 0, 2, 0, 1, 0, 0, 0, 0 },
+          tileid = 167
+        },
+        {
+          wangid = { 0, 2, 0, 1, 0, 1, 0, 2 },
+          tileid = 168
+        },
+        {
+          wangid = { 0, 0, 0, 0, 0, 1, 0, 2 },
+          tileid = 169
+        }
+      }
+    }
+  },
   tilecount = 170,
   tiles = {}
 }
--- scripts/world/tilesets/nowhere.tsx
+++ scripts/world/tilesets/nowhere.tsx
@@ -1,4 +1,48 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.11" tiledversion="1.11.2" name="nowhere" tilewidth="20" tileheight="20" tilecount="170" columns="17" tilerendersize="grid">
  <image source="../../../assets/sprites/tilesets/nowhere.png" width="340" height="200"/>
+ <wangsets>
+  <wangset name="Base Tiles" type="corner" tile="-1">
+   <wangcolor name="Turfle" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Blackglass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="14" wangid="0,0,0,2,0,1,0,0"/>
+   <wangtile tileid="15" wangid="0,0,0,1,0,2,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="22" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="31" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="34" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="35" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="36" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="38" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="39" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="48" wangid="0,2,0,0,0,0,0,1"/>
+   <wangtile tileid="49" wangid="0,1,0,0,0,0,0,2"/>
+   <wangtile tileid="85" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="86" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="87" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="102" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="103" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="104" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="106" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="107" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="119" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="120" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="121" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="123" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="124" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="150" wangid="0,1,0,2,0,0,0,0"/>
+   <wangtile tileid="151" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="152" wangid="0,0,0,0,0,2,0,1"/>
+   <wangtile tileid="167" wangid="0,2,0,1,0,0,0,0"/>
+   <wangtile tileid="168" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="169" wangid="0,0,0,0,0,1,0,2"/>
+  </wangset>
+ </wangsets>
 </tileset>
